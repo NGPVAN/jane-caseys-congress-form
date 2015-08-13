375d97d9d665f2f91e625aec130c7692012b36c6
Stephen Shepherd <SShepherd@ngpvan.com>
2015-08-13 14:04:23 -0400

    consolidate submissions

--- middleware/submitFormData.js
+++ middleware/submitFormData.js
@@ -6,5 +6,10 @@ module.exports = function(req, res) {
   var params = req.body;
   crud.dbInsert(params);
 
-  res.render(VIEWS_DIR + 'success');
+  if(req.query.alertBox != null) {
+    res.render(VIEWS_DIR + req.body.bioid,{triggerAlert:true, bioId: req.body.bioid});
+  }
+  else {
+    res.render(VIEWS_DIR + 'success');
+  }
 };
