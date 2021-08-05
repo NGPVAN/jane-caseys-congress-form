ad62921f56a7b287fa99c02b5abab427de186f3d
Tommy Irish <tirish@ngpvan.com>
2021-08-05 16:46:16 -0400

    VAN-82545: add fake cwc routes, remove bodyparser

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 var express = require('express');
-var bodyParser = require('body-parser');
 var path = require('path');
 var log = require('./lib/log');
 
@@ -10,9 +9,14 @@ var routes = require('./lib/routes');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.raw({
+  // XML workaround
+  type: function(req){
+    return req.headers['content-type'] && req.headers['content-type'].toLowerCase().includes('xml')
+  }
+}));
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/bower_components',
